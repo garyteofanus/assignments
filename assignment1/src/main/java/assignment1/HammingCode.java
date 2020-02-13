bba582f2af185a6aa6268d226536c87c82106d9e
Muhammad Fairuzi Teguh <mfairuzit@gmail.com>
2020-02-13 17:57:24 +0700

    Initialize assignment 1

---
+++ assignment1/src/main/java/assignment1/HammingCode.java
@@ -0,0 +1,49 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class HammingCode {
+    static final int ENCODE_NUM = 1;
+    static final int DECODE_NUM = 2;
+    static final int EXIT_NUM = 3;
+    
+    public String encode(String data) {
+        return "TODO";
+    }
+    
+    public String decode(String code) {
+        return "TODO";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di program Hamming Code!");
+        System.out.println("=======================================");
+        HammingCode hammingCode = new HammingCode();
+        Scanner in = new Scanner(System.in);
+        boolean hasChosenExit = false;
+        while (!hasChosenExit) {
+            System.out.println();
+            System.out.println("Pilih operasi:");
+            System.out.println("1. Encode");
+            System.out.println("2. Decode");
+            System.out.println("3. Exit");
+            System.out.println("Masukkan nomor operasi yang diinginkan: ");
+            int operation = in.nextInt();
+            if (operation == ENCODE_NUM) {
+                System.out.println("Masukkan data: ");
+                String data = in.next();
+                String code = hammingCode.encode(data);
+                System.out.println("Code dari data tersebut adalah: " + code);
+            } else if (operation == DECODE_NUM) {
+                System.out.println("Masukkan code: ");
+                String code = in.next();
+                String data = hammingCode.decode(code);
+                System.out.println("Data dari code tersebut adalah: " + data);
+            } else if (operation == EXIT_NUM) {
+                System.out.println("Sampai jumpa!");
+                hasChosenExit = true;
+            }
+        }
+        in.close();
+    }
+}
