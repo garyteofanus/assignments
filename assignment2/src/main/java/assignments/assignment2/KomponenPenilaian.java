84bfe3d06eff8e715fa1d506d31679721de33824
Sage Muhammad Abdullah <laymonage@gmail.com>
2020-03-02 18:33:53 +0700

    Create starter code for assignment2

--- assignment2/src/main/java/assignments/assignment2/KomponenPenilaian.java
+++ assignment2/src/main/java/assignments/assignment2/KomponenPenilaian.java
@@ -1,5 +1,65 @@
 package assignments.assignment2;
 
 public class KomponenPenilaian {
+    private String nama;
+    private ButirPenilaian[] butirPenilaian;
+    private int bobot;
+
+    public KomponenPenilaian(String nama, int banyakButirPenilaian, int bobot) {
+        // TODO: buat constructor untuk KomponenPenilaian.
+        // Note: banyakButirPenilaian digunakan untuk menentukan panjang butirPenilaian saja
+        // (tanpa membuat objek-objek ButirPenilaian-nya).
+    }
+
+    /**
+     * Membuat objek KomponenPenilaian baru berdasarkan bentuk KomponenPenilaian templat.
+     * @param templat templat KomponenPenilaian.
+     */
+    private KomponenPenilaian(KomponenPenilaian templat) {
+        this(templat.nama, templat.butirPenilaian.length, templat.bobot);
+    }
+
+    /**
+     * Mengembalikan salinan skema penilaian berdasarkan templat yang diberikan.
+     * @param templat templat skema penilaian sebagai sumber.
+     * @return objek baru yang menyerupai templat.
+     */
+    public static KomponenPenilaian[] salinTemplat(KomponenPenilaian[] templat) {
+        KomponenPenilaian[] salinan = new KomponenPenilaian[templat.length];
+        for (int i = 0; i < salinan.length; i++) {
+            salinan[i] = new KomponenPenilaian(templat[i]);
+        }
+        return salinan;
+    }
+
+    public void masukkanButirPenilaian(int idx, ButirPenilaian butir) {
+        // TODO: masukkan butir ke butirPenilaian pada index ke-idx.
+    }
+
+    public String getNama() {
+        // TODO: kembalikan nama KomponenPenilaian.
+        return "";
+    }
+
+    public double getRerata() {
+        // TODO: kembalikan rata-rata butirPenilaian.
+        return 0;
+    }
+
+    public double getNilai() {
+        // TODO: kembalikan rerata yang sudah dikalikan dengan bobot.
+        return 0;
+    }
+
+    public String getDetail() {
+        // TODO: kembalikan detail KomponenPenilaian sesuai permintaan soal.
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        // TODO: kembalikan representasi String sebuah KomponenPenilaian sesuai permintaan soal.
+        return "";
+    }
 
 }
--- assignment2/src/main/java/assignments/assignment2/Mahasiswa.java
+++ assignment2/src/main/java/assignments/assignment2/Mahasiswa.java
@@ -1,5 +1,70 @@
 package assignments.assignment2;
 
-public class Mahasiswa {
+public class Mahasiswa implements Comparable<Mahasiswa> {
+    private String npm;
+    private String nama;
+    private KomponenPenilaian[] komponenPenilaian;
 
+    public Mahasiswa(String npm, String nama, KomponenPenilaian[] komponenPenilaian) {
+        // TODO: buat constructor untuk Mahasiswa.
+        // Note: komponenPenilaian merupakan skema penilaian yang didapat dari GlasDOS.
+    }
+
+    public KomponenPenilaian getKomponenPenilaian(String namaKomponen) {
+        // TODO: kembalikan KomponenPenilaian yang bernama namaKomponen.
+        // Note: jika tidak ada, kembalikan null atau lempar sebuah Exception.
+        return null;
+    }
+
+    public String getNpm() {
+        // TODO: kembalikan NPM mahasiswa.
+        return "";
+    }
+
+    /**
+     * Mengembalikan huruf berdasarkan nilai yang diberikan.
+     * @param nilaiAkhir nilai untuk dicari hurufnya.
+     * @return huruf dari nilai.
+     */
+    private static String getHuruf(double nilai) {
+        return nilai >= 85 ? "A" :
+            nilai >= 80 ? "A-" :
+            nilai >= 75 ? "B+" :
+            nilai >= 70 ? "B" :
+            nilai >= 65 ? "B-" :
+            nilai >= 60 ? "C+" :
+            nilai >= 55 ? "C" :
+            nilai >= 40 ? "D" : "E";
+    }
+
+    /**
+     * Mengembalikan status kelulusan berdasarkan nilaiAkhir yang diberikan.
+     * @param nilaiAkhir nilai akhir mahasiswa.
+     * @return status kelulusan (LULUS/TIDAK LULUS).
+     */
+    private static String getKelulusan(double nilaiAkhir) {
+        return nilaiAkhir >= 55 ? "LULUS" : "TIDAK LULUS";
+    }
+
+    public String rekap() {
+        // TODO: kembalikan rekapan sesuai dengan permintaan soal.
+        return "";
+    }
+
+    public String toString() {
+        // TODO: kembalikan representasi String dari Mahasiswa sesuai permintaan soal.
+        return "";
+    }
+
+    public String getDetail() {
+        // TODO: kembalikan detail dari Mahasiswa sesuai permintaan soal.
+        return "";
+    }
+
+    @Override
+    public int compareTo(Mahasiswa other) {
+        // TODO: definisikan cara membandingkan seorang mahasiswa dengan mahasiswa lainnya.
+        // Hint: bandingkan NPM-nya, String juga punya method compareTo.
+        return 0;
+    }
 }
